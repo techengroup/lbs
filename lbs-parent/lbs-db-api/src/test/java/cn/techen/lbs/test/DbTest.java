7c61180dfd12df53d6e0eba63fd8a78c5d16f833
kimcash <zhangyue@techen.cn>
2018-07-10 15:22:39 +0800

    import sql

--- lbs-parent/lbs-db-api/src/test/java/cn/techen/lbs/test/DbTest.java
+++ lbs-parent/lbs-db-api/src/test/java/cn/techen/lbs/test/DbTest.java
@@ -1,8 +1,5 @@
 package cn.techen.lbs.test;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +7,7 @@ import cn.techen.lbs.db.common.GlobalUtil;
 import cn.techen.lbs.db.sql.AbstractSQL;
 import junit.framework.TestCase;
 
-public class BusinessTest extends TestCase {
+public class DbTest extends TestCase {
 	private static Logger log = (Logger) LoggerFactory.getLogger("Lora-Protocol");
 	
 	public void test() {
@@ -19,15 +16,8 @@ public class BusinessTest extends TestCase {
 
 	public void testA() {
 		try {
-			AbstractSQL as = GlobalUtil.newSql("Fn1000043");
-			Queue<Object> datas = new LinkedList<Object>();
-			datas.add("10.10.1.1");
-			datas.add("9010");
-			datas.add("10.10.1.1");
-			datas.add("9010");
-			datas.add("cnnet");
-			String sql = as.handle(0, datas);
-			log.info(sql);
+			AbstractSQL as = GlobalUtil.newSql("Fn1000043");	
+			log.info(as.toString());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
